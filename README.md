1b51098cbcfbcd515f7f8d5f16cf13df985b07cb
LucasParodi00 <lucasparodi00@gmail.com>
2026-01-04 20:21:43 -0300

    Se agrega el porfolio 2026

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Porfolio Parodi Lucas Ivan
+# Portfolio Parodi Lucas Ivan.
+
+### Poroflio 2026 - NextJS - TailwindCss - Shadcn
+
+### Porfolio 2025 - Astro - TailwindCss
--- src/components/navbar/NavbarItem.jsx
+++
@@ -1,9 +0,0 @@
-export const NavbarItem = ({ referencia = "#", texto = "Texto", patchName = '/' }) => {
-	return (
-		<a
-			href={referencia}
-			className={`uppercase text-lg  duration-500 ${patchName === 'active' ? 'text-rosado' : 'hover:text-rosado'}`}>
-			{texto}
-		</a>
-	);
-};
